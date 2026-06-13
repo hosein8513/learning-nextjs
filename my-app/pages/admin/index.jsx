22eb01bc155c70af87c67695be612527beb29e39
hosein <kazmyh279@gmail.com>
2026-06-13 19:54:05 +0330

    use-swr

---
+++ my-app/pages/admin/index.jsx
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+const index = () => {
+    const router = useRouter()
+    const [data, setData] = useState({})
+    const [loading, setLoading] = useState(false)
+
+    const handleGetData = async () => {
+        setLoading(true)
+        const res = await fetch('http://localhost:3000/admin')
+        const resData = await res.json()
+        setData(resData)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        handleGetData()
+    }, [])
+    return loading ? (
+        <h1 className='text-center p-4'>Loading...</h1>
+    ) : (
+        <>
+            <button className='p-4 rounded-lg bg-gray-300' onClick={() => router.back()}>back</button>
+            <ul>
+                {Object.keys(data).map(key => (
+                    <li>{`${key} : ${data[key]}`}</li>
+                ))}
+            </ul>
+
+        </>
+    );
+};
+
+export default index;
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -4,8 +4,10 @@ import React from 'react';
 const index = () => {
   return (
     <div className='w-full h-full flex flex-col justify-center items-center'>
-      <Link className='w-[40px] h-7.5 text-center bg-blue-500 text-white' href={'/posts'}>Show Posts</Link>
-      <Link className='w-[40px] h-7.5 text-center bg-blue-500 text-white' href={'/users'}>Show users</Link>
+      <Link className='w-[80px] h-7.5 text-center bg-blue-500 text-white' href={'/posts'}>Show Posts</Link>
+      <Link className='w-[80px] h-7.5 text-center bg-blue-500 text-white' href={'/users'}>Show users</Link>
+      <Link className='w-[80px] h-7.5 text-center bg-blue-500 text-white' href={'/admin'}>Show admin</Link>
+      <Link className='w-[80px] h-7.5 text-center bg-blue-500 text-white' href={'/admin/panel'}>Show admin-panel</Link>
     </div>
   );
 };
