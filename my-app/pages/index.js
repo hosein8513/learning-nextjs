ce2aa49ad5cf1791b27da06da994365b7dcd706f
hosein <kazmyh279@gmail.com>
2026-06-13 10:42:29 +0330

    use-ssr

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -3,8 +3,9 @@ import React from 'react';
 
 const index = () => {
   return (
-    <div className='w-full h-full flex justify-center items-center'>
-      <Link className='w-12.5 h-7.5 text-center bg-blue-500 text-white' href={'/posts'}>Show Posts</Link>
+    <div className='w-full h-full flex flex-col justify-center items-center'>
+      <Link className='w-[40px] h-7.5 text-center bg-blue-500 text-white' href={'/posts'}>Show Posts</Link>
+      <Link className='w-[40px] h-7.5 text-center bg-blue-500 text-white' href={'/users'}>Show users</Link>
     </div>
   );
 };
