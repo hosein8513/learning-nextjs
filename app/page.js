e30617bd722761e92a1aecea64faa97d61a90f15
hosein <kazmyh279@gmail.com>
2026-07-02 19:54:23 +0330

    use-intercepting-routes

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ const HomePage = () => {
         <div className='flex flex-col justify-center items-center h-full'>
             <h1 className='my-5'>صفحه اصلی</h1>
             <CustomButton className='bg-green-400 w-64' onClick={()=>router.push("/login")}>صفحه ورود</CustomButton>
+             <CustomButton className='bg-pink-400 w-64 mt-10' onClick={()=>router.push("/about")}>درباره ما</CustomButton>
         </div>
     );
 }
